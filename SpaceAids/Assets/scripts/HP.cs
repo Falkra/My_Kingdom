51958271ff0088ac339e416f56a22c13e94dd6fb
Falkra <justinudo959@gmail.com>
2018-06-22 09:51:31 +0200

    first commit

---
+++ SpaceAids/Assets/scripts/HP.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour {
+
+    public bool playerIsInHitbox = false;
+    public bool isPlayer1 = true;
+    public Text HealthDisplay;
+    public int maxHP;
+
+    public int damegeIndicator = 5;
+    public int hitPoints;
+
+    public KeyCode hit;
+
+	// Use this for initialization
+	void Start () {
+        HealthDisplay.text = hitPoints + "/" + maxHP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(hit) && playerIsInHitbox)
+        {
+            hitPoints = hitPoints - damegeIndicator;
+            HealthDisplay.text = hitPoints + "/" + maxHP + "HP";
+            
+            
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "P1" && !isPlayer1)
+        {
+            playerIsInHitbox = true;
+        }
+
+        if (other.tag == "P2" && isPlayer1)
+        {
+            playerIsInHitbox = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "P1" && !isPlayer1)
+        {
+            playerIsInHitbox = false;
+        }
+
+        if (other.tag == "P2" && isPlayer1)
+        {
+            playerIsInHitbox = false;
+        }
+    }
+}
---
+++ SpaceAids/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public KeyCode jumpKey;
+    public KeyCode moveLeftKey;
+    public KeyCode moveRightKey;
+
+
+    public int jumpHeight = 13;
+    public bool _isonground = true;
+
+    private Rigidbody2D _rigidBody;
+    // Use this for initialization
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(jumpKey) && _isonground)
+        {
+            _rigidBody.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+            _isonground = false;
+        }
+
+        if (Input.GetKey(moveLeftKey))
+        {
+            transform.Translate (-10f * Time.deltaTime, 0f, 0f );
+        }
+
+        if (Input.GetKey(moveRightKey))
+        {
+            transform.Translate (10f * Time.deltaTime, 0f, 0f);
+        }
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            _isonground = true;
+
+        }
+    }
+}
